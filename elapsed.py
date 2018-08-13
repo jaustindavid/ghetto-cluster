127c4e3e0c0f6dbf8969e8a3f228bd76fa967876
Austin David <austin@austindavid.com>
2018-08-13 11:23:36 -0400

    adding a once_every(nSeconds)

--- elapsed.py
+++ elapsed.py
@@ -1,5 +1,24 @@
 #! python3.x
 
+"""
+usage:
+    from elapsed import ElapsedTimer
+    ...
+    timer = ElapsedTimer()
+    while True:
+        print(f"It's been {timer.elapsed()}s since start")
+        if timer.once_every(10):
+            print("This happens only once every 10s")
+        time.sleep(1)
+
+    ...
+    timer.reset() 
+    print(f"0: {int(timer.elapsed())}; True: {timer.once_every(5)}")
+    print(f"0: {int(timer.elapsed())}; False: {timer.once_every(5)}")
+    time.sleep(5)
+    print(f"5: {int(timer.elapsed())}; True: {timer.once_every(5)}")
+"""
+
 import time
 
 class ElapsedTimer:
@@ -11,3 +30,21 @@ class ElapsedTimer:
 
     def reset(self):
         self.start = time.time()
+        self.last = 0
+
+    def once_every(self, interval):
+        if (self.last + interval) < time.time():
+            self.last = time.time()
+            return True
+        return False
+
+
+if __name__ == "__main__":
+    from elapsed import ElapsedTimer
+
+    timer = ElapsedTimer()
+    while timer.elapsed() < 5:
+        print(f"It's been {timer.elapsed():3.1f}s since start")
+        if timer.once_every(2):
+            print("This happens only once every 2s")
+        time.sleep(1)
--- elapsed_test.py
+++ elapsed_test.py
@@ -14,12 +14,17 @@ class TestTimerMethods(unittest.TestCase):
     def test_timer(self):
         start = int(time.time())
         timer = elapsed.ElapsedTimer()
+        self.assertEquals(timer.once_every(10), True)
         time.sleep(3)
         self.assertEquals(int(timer.elapsed()), 3)
         timer.reset()
         self.assertEquals(int(timer.elapsed()), 0)
         time.sleep(3)
         self.assertEquals(int(timer.elapsed()), 3)
+        print(f"So far only {time.time() - start}s elapsed")
+        self.assertEquals(timer.once_every(10), False)
+        time.sleep(4)
+        self.assertEquals(timer.once_every(10), True)
 
 
 if __name__ == "__main__":
